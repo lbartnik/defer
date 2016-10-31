6d54c9b349791291ea870111f47d0202a82512d0
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-10-30 19:42:21 -0700

    start a new package

---
+++ R/package.R
@@ -0,0 +1,14 @@
+
+#' Create an execution package.
+#'
+#' @param entry Entry-point function.
+#' @param ... Other function to be packaged.
+#' @param .funcs A list of functions.
+#' @return An execution package object.
+#'
+#' @export
+#'
+package_ <- function (entry, ..., .funcs)
+{
+
+}
