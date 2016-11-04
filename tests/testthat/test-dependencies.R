3e293a39e9f3eba7aad7f8e67d00bcdec5e027cf
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-11-03 22:39:09 -0700

    add test for future dependency extraction

---
+++ tests/testthat/test-dependencies.R
@@ -0,0 +1,23 @@
+# --- various types of functions ---------------------------------------
+
+test_that("regular function can be packaged", {
+  pkg <- package_(function(x)summary(x), .extract = TRUE)
+  expect_true(is_execution_package(pkg))
+  expect_equal(list_dependencies(pkg, c(base = 'summary')))
+
+  ret <- local(run_package(pkg, iris), envir = globalenv())
+  expect_equal(ret, summary(iris))
+})
+
+
+test_that("%>% can be packaged", {
+  skip_if_not_installed("magrittr")
+
+  # TODO make sure dependencies are extracted
+  pkg <- package_(. %>% summary(.), .extract = TRUE)
+  expect_true(is_execution_package(pkg))
+  expect_equal(list_dependencies(pkg, c(base = 'summary')))
+
+  ret <- local(run_package(pkg, iris), envir = globalenv())
+  expect_equal(ret, summary(iris))
+})
