ed4d66d87825e46da15eb22c1f9feacd42ebe5d1
Lukasz A. Bartnik <l.bartnik@gmail.com>
2017-05-29 23:33:49 -0700

    reorganize tests

--- tests/testthat/test-extras.R
+++ tests/testthat/test-extras.R
@@ -24,3 +24,16 @@ test_that("library functions are not packaged but recorded", {
 })
 
 
+test_that("%>% can be packaged", {
+  skip("not implemented yet")
+  skip_if_not_installed("magrittr")
+  
+  # TODO make sure dependencies are extracted
+  pkg <- package_(. %>% summary(.), .extract = TRUE)
+  expect_true(is_execution_package(pkg))
+  expect_equal(list_dependencies(pkg, c(base = 'summary')))
+  
+  ret <- local(run_package(pkg, iris), envir = globalenv())
+  expect_equal(ret, summary(iris))
+})
+
